5dc90337aa1e8db2f03ca59051a8910eae9194bb
Sharonne24 <sharonawuor1995>
2023-04-29 09:49:00 +0300

    fetching menu details using public API

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+const apiUrl = ('https://raw.githubusercontent.com/Sharonne24/independent-website/main/db.json');
+async function getMenu() {
+    const response = await fetch('https://raw.githubusercontent.com/Sharonne24/independent-website/main/db.json');
+    const data = await response.json();
+    const menuContainer = document.getElementById('menu-container');
+    data.menu.forEach(item => {
+
+      const menuItem = document.createElement('div');
+      menuItem.classList.add('menu-item');
+
+      const menuItemInfo = document.createElement('div');
+      menuItemInfo.classList.add('menu-item-info');
+
+      const name = document.createElement('h3');
+      name.textContent = item.name;
+
+      const description = document.createElement('p');
+      description.textContent = item.description;
+
+      const price = document.createElement('p');
+      price.classList.add('price');
+
+      price.textContent = `$${item.price.toFixed(2)}`;
+      const image = document.createElement('img');
+
+      image.src = item.image;
+      image.alt = item.name;
+      menuItemInfo.appendChild(name);
+      menuItemInfo.appendChild(description);
+      menuItemInfo.appendChild(price);
+      menuItem.appendChild(image);
+      menuItem.appendChild(menuItemInfo);
+      menuContainer.appendChild(menuItem);
+    });
+  }
+
+  getMenu();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const orderForm = document.querySelector('#order-form');
 
 orderForm.addEventListener('submit', (event) => {

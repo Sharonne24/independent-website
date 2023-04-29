b746e80effc6fad907c2f49c85b2cf3df4fbcd4d
Sharonne24 <sharonawuor1995>
2023-04-29 09:02:52 +0300

    reservation form

---
+++ index.js
@@ -0,0 +1,44 @@
+//order form 
+const orderForm = document.querySelector('#order-form');
+
+orderForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const name = document.getElementById('name').reset();
+  const email = document.querySelector('#email').value.trim();
+  const phone = document.querySelector('#phone').value.trim();
+  const address = document.querySelector('#address').value.trim();
+  const order = document.querySelector('#order').value.trim();
+
+  if (!name || !email || !phone || !address || !order) {
+    alert('Please fill all the fields.');
+    return;
+  }
+
+  // send the form data to the server
+  fetch('/submit-order', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      phone: phone,
+      address: address,
+      order: order
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {
+    if (response.ok) {
+      alert('Order submitted successfully!');
+      orderForm.reset(); // Clear the form
+    } else {
+      alert('Something went wrong. Please try again later.');
+    }
+  })
+  .catch(error => {
+    alert('Something went wrong. Please try again later.');
+  });
+});
+  
